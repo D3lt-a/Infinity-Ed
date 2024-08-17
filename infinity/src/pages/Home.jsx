db744c839d7f2accd5a56dba67bc2ec4ca4b45c9
Gavin <kirengaboris07@gmail.com>
2024-08-17 02:34:09 -0900

    Home Page begining

--- infinity/src/pages/Home.jsx
+++ infinity/src/pages/Home.jsx
@@ -1,8 +1,24 @@
+// import pic from '../assets/images/home-back.png'
 
 function Home() {
   return (
     <div>
       <h1>HOME PAGE</h1>
+      
+
+<section className="bg-center bg-no-repeat bg-gray-700 bg-blend-multiply">
+    <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Unlock the Power of Technology</h1>
+        <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Innovate , Naviagte , Elevate</p>
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+        <button type="button" className="text-yellow-400 hover:text-white border-2 py-3 px-5 sm:ms-4 font-medium border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium text-sm px text-center me-2 mb-2 dark:border-orange-500 dark:text-orange-500 dark:hover:text-white dark:hover:bg-orange-500 dark:focus:ring-orange-500">Reach Us</button>
+            <a href="#" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                Learn more
+            </a>  
+        </div>
+    </div>
+</section>
+
     </div>
   )
 }
